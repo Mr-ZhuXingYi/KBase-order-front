624bf087096ff8c5ccdda82e21d30b7a00322ac7
zxy <zhuxingyi83@163.com>
2021-01-27 09:34:53 +0800

    commit

---
+++ src/views/Order.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="order">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/Order.vue'
+
+export default {
+  name: 'Order',
+  components: {
+    HelloWorld
+  }
+}
+</script>
